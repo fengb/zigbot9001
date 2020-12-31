6ae6a100aca11fc6d7f5a67faa7d582eb1db0869
Benjamin Feng <benjamin.feng@glassdoor.com>
2020-12-31 17:37:59 -0600

    iguanaTLS

--- src/request.zig
+++ src/request.zig
@@ -1,23 +1,21 @@
 const std = @import("std");
 const hzzp = @import("hzzp");
-const ssl = @import("zig-bearssl");
+// const ssl = @import("zig-bearssl");
+const tls = @import("iguanaTLS");
 
 const bot_agent = "zigbot9001/0.0.1";
 
 pub const SslTunnel = struct {
     allocator: *std.mem.Allocator,
 
-    trust_anchor: ssl.TrustAnchorCollection,
-    x509: ssl.x509.Minimal,
-    client: ssl.Client,
+    // trust_anchor: ssl.TrustAnchorCollection,
+    // x509: ssl.x509.Minimal,
+    // client: ssl.Client,
 
     tcp_conn: std.fs.File,
-    tcp_reader: std.fs.File.Reader,
-    tcp_writer: std.fs.File.Writer,
-
     conn: Stream,
 
-    pub const Stream = ssl.Stream(*std.fs.File.Reader, *std.fs.File.Writer);
+    pub const Stream = tls.Client(std.fs.File.Reader, std.fs.File.Writer);
 
     pub fn init(args: struct {
         allocator: *std.mem.Allocator,
@@ -30,29 +28,33 @@ pub const SslTunnel = struct {
 
         result.allocator = args.allocator;
 
-        result.trust_anchor = ssl.TrustAnchorCollection.init(args.allocator);
-        errdefer result.trust_anchor.deinit();
-        try result.trust_anchor.appendFromPEM(args.pem);
+        // TODO: restore cert validation
+        // result.trust_anchor = ssl.TrustAnchorCollection.init(args.allocator);
+        // errdefer result.trust_anchor.deinit();
+        // try result.trust_anchor.appendFromPEM(args.pem);
 
-        result.x509 = ssl.x509.Minimal.init(result.trust_anchor);
-        result.client = ssl.Client.init(result.x509.getEngine());
-        result.client.relocate();
-        try result.client.reset(args.host, false);
+        // result.x509 = ssl.x509.Minimal.init(result.trust_anchor);
+        // result.client = ssl.Client.init(result.x509.getEngine());
+        // result.client.relocate();
+        // try result.client.reset(args.host, false);
 
         result.tcp_conn = try std.net.tcpConnectToHost(args.allocator, args.host, args.port);
         errdefer result.tcp_conn.close();
 
-        result.tcp_reader = result.tcp_conn.reader();
-        result.tcp_writer = result.tcp_conn.writer();
-
-        result.conn = ssl.initStream(result.client.getEngine(), &result.tcp_reader, &result.tcp_writer);
+        result.conn = try tls.client_connect(.{
+            .reader = result.tcp_conn.reader(),
+            .writer = result.tcp_conn.writer(),
+            .cert_verifier = .none,
+        }, args.host);
+        errdefer result.conn.close_notify() catch {};
 
         return result;
     }
 
     pub fn deinit(self: *SslTunnel) void {
+        self.conn.close_notify() catch {};
         self.tcp_conn.close();
-        self.trust_anchor.deinit();
+        // self.trust_anchor.deinit();
 
         self.allocator.destroy(self);
     }
@@ -64,7 +66,7 @@ pub const Https = struct {
     buffer: []u8,
     client: HzzpClient,
 
-    const HzzpClient = hzzp.base.Client.Client(SslTunnel.Stream.DstInStream, SslTunnel.Stream.DstOutStream);
+    const HzzpClient = hzzp.base.Client.Client(SslTunnel.Stream.Reader, SslTunnel.Stream.Writer);
 
     pub fn init(args: struct {
         allocator: *std.mem.Allocator,
@@ -85,7 +87,7 @@ pub const Https = struct {
         const buffer = try args.allocator.alloc(u8, 0x1000);
         errdefer args.allocator.free(buffer);
 
-        var client = hzzp.base.Client.create(buffer, ssl_tunnel.conn.inStream(), ssl_tunnel.conn.outStream());
+        var client = hzzp.base.Client.create(buffer, ssl_tunnel.conn.reader(), ssl_tunnel.conn.writer());
 
         try client.writeHead(args.method, args.path);
 
@@ -116,7 +118,7 @@ pub const Https = struct {
         try self.client.writeHeadComplete();
 
         try self.client.writer.print(fmt, args);
-        try self.ssl_tunnel.conn.flush();
+        //try self.ssl_tunnel.conn.flush();
     }
 
     pub fn expectSuccessStatus(self: *Https) !u16 {
